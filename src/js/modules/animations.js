b809c857cd0e1ffbe92df56acc98128cee15ebb7
Mark LaBarbara <mlabarbara@gmail.com>
2024-12-18 12:06:58 -0700

    remore src folder and add some header changes to msu-project.html

--- src/js/modules/animations.js
+++
@@ -1,6 +0,0 @@
-export function initializeAnimations() {
-    const animatedElements = document.querySelectorAll('.animate-fade-in');
-    animatedElements.forEach((element, index) => {
-        element.style.animationDelay = `${index * 0.2}s`;
-    });
-}
